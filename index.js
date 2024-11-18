3c79d3dbcd2424d37f887ea1a189bfcd8afc8b24
Norrheden <eliasnorrhedenmau@gmail.com>
2024-11-18 13:25:16 +0100

    cityBoxes + true/false const
    Added a for loop that loops every city name in cities and assign it with a div witch a className and append it to citiesTable. CityFound checks if we find a city in f1 and if it did cityFound will be true.

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 // Recommended: All functions declared here
+function f1(target) {
+    for (let key in cities) {
+        if (target == cities[key].name) {
+            h2.textContent = `${target} (${cities[key].country})`;
+            title.textContent = `${target}`
+            return cityFound = true;
+            break;
+        } else {
+            h2.textContent = `${target} finns inte i databasen`;
+            title.textContent = `Not Found`
+        }
+    }
+}
 
 // Recommended: constants with references to existing HTML-elements
-
+const title = document.querySelector("title");
 const h2 = document.querySelector("h2");
 const h3 = document.querySelector("h3");
+const citiesTable = document.getElementById("cities")
 
 // Recommended: Ask for the city name and then the rest of the code
 
 const targetCityName = prompt("Vilken stad");
+let cityFound = false;
 
+f1(targetCityName);
+
+
+if (cityFound == false) {
+    h3.textContent = "";
+}
 
 
 for(let key in cities) {
-    console.log(cities[key].name);
+    const div = document.createElement("div")
+    div.className = "cityBox"
+    div.textContent = cities[key].name;
+    citiesTable.appendChild(div);
 }
 
 
-for (let key in cities) {
-    if (targetCityName.toLocaleLowerCase() == cities[key].name.toLowerCase()) {
-        h2.textContent = `${targetCityName} (${cities[key].country})`;
-        break;
-    } else {
-        h2.textContent = `${targetCityName} finns inte i databasen`;
-        h3.textContent = "";
 
-    }
-}
+
+
 
